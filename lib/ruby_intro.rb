dec984f8ab9cf5c0abb0538d479f0de1800437af
Ubuntu <ubuntu@ip-172-31-25-226.us-east-2.compute.internal>
2020-01-03 07:55:31 +0000

    Finished Code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,90 @@
 
 def sum arr
   # YOUR CODE HERE
+	res = 0
+	arr.each { |a| res += a}
+	res
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+	sum = 0
+	maxes = arr.max(2)
+	sum = maxes.sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+	arr.combination(2) { |c| return true if c.sum == n }
+	false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  res = "Hello, "
+  res << name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  Regexp.new('^[qwrtypsdfghjklzxcvbnm]', Regexp::IGNORECASE).match?(s)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	#if /^[01]+$/.match?(s)
+	#	s.to_i(2) % 4 == 0
+	#else
+	#	false
+	#end
+	/^(0|00|([01]+00))$/.match?(s)
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+	def initialize(isbn, price)
+		if isbn.empty?
+			raise ArgumentError.new("Empty String")
+		end
+		if price <= 0
+			raise ArgumentError.new("Price is less than or equal to zero")
+		end
+		@isbn = isbn
+		@price = price
+	end
+
+	def isbn
+		@isbn
+	end
+
+	def price
+		@price
+	end
+
+	def price=(price)
+		@price = price
+	end
+
+	def isbn=(isbn)
+		@isbn = isbn
+	end
+
+	def price_as_string()
+		res = "$"
+		decimals = @price - @price.floor		
+		if (decimals > 0)
+		    firstPart = @price - decimals
+		    firstPart = firstPart.to_s
+		    res << firstPart.sub!(/.0$/, "")
+		    res << "."
+		    decimals = (decimals * 100)
+		    decimals = decimals.floor
+		    decimalString = decimals.to_s
+		    res << decimalString
+		else
+			res << (@price.to_s)
+			res << ".00"
+		end
+		res
+	end
+
 end
